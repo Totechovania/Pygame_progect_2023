53be4cd156c24b0f6818021b5e4e190c34726170
Totechovania <bi068612@gmail.com>
2023-12-28 15:59:20 +0300

    TestFrame edited

--- Frames/TestFrame.py
+++ Frames/TestFrame.py
@@ -2,18 +2,31 @@ from IFrame import IFrame
 import pygame as pg
 from Signals import *
 import shared
+import time
 
 
 class TestFrame(IFrame):
-    def __init__(self, parent: str = 'None'):
-        self.font = pg.font.Font(None, 50)
-        self.parent = parent
+    font = None
+    annotation_text = None
+    history_text = None
+
+    def __init__(self, previous: str = 'TestFrame', n=0):
+        if TestFrame.font is None:
+            TestFrame.font = pg.font.Font(None, 50)
+        if TestFrame.annotation_text is None:
+            TestFrame.annotation_text = self.font.render(f'Esc - kill top frame, N - new frame', True, pg.Color('white'))
+        if TestFrame.history_text is None:
+            TestFrame.history_text = self.font.render('History:', True, pg.Color('white'))
+
+        self.n = n
+        self.previous = previous
+        self.history = []
 
     def begin(self):
-        print('begin')
+        self.history.append(f'{time.strftime("%H:%M:%S")} - begin')
 
     def end(self):
-        print('end')
+        self.history.append(f'{time.strftime("%H:%M:%S")} - end')
 
     def update(self):
         shared.screen.fill(pg.Color('black'))
@@ -22,14 +35,26 @@ class TestFrame(IFrame):
         for event in events:
             if event.type == pg.QUIT:
                 raise KillEntireApp
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    raise KillTopFrame
+                if event.key == pg.K_n:
+                    raise NewFrame(TestFrame(n=self.n + 1))
 
-        text = self.font.render(f'Parent: {self.parent}', True, pg.Color('white'))
-        shared.screen.blit(text, (0, 0))
+        previous_text = self.font.render(f'Previous: {self.previous}{self.n}', True, pg.Color('white'))
+        shared.screen.blit(previous_text, (0, 0))
+
+        shared.screen.blit(self.history_text, (500, 0))
+        for i, event in enumerate(self.history):
+            text = self.font.render(event, True, pg.Color('white'))
+            shared.screen.blit(text, (500, 50 * (i + 1)))
+
+        shared.screen.blit(self.annotation_text, (0, shared.HEIGHT - 100))
 
         pg.display.flip()
 
     def pause(self):
-        print('pause')
+        self.history.append(f'{time.strftime("%H:%M:%S")} - pause')
 
     def resume(self):
-        print('resume')
+        self.history.append(f'{time.strftime("%H:%M:%S")} - resume')
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from FrameController import FrameController
 from utilities import apply_global_settings, set_shared_variables, terminate
-from Frames.MainMenu import MainMenu
+from Frames.TestFrame import TestFrame
 
 
 if __name__ == '__main__':
     apply_global_settings()
     set_shared_variables()
 
-    main_menu = MainMenu() # Здесь должен быть фрейм, отвечающий за главное меню(или заставку).
+    main_menu = TestFrame() # Здесь должен быть фрейм, отвечающий за главное меню(или заставку).
 
     controller = FrameController(main_menu)
     controller.run()
