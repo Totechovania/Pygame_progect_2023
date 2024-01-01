13d74f336f89733adf9e0fc3e5b4e073dfd61bbc
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-01 18:32:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from FrameController import FrameController
-from Frames.ButtonDemonstratinon import ButtonDemonstration
 from utilities import apply_global_settings, set_shared_variables, terminate
-
+from Frames.MainMenu import MainMenu
 
 
 if __name__ == '__main__':
     apply_global_settings()
     set_shared_variables()
 
-    #main_menu = TestFrame(supposed_to_be='Пример работы с фреймами.') # Здесь должен быть фрейм, отвечающий за главное меню(или заставку).
-    main_menu = ButtonDemonstration()
+    main_menu = MainMenu()
 
     controller = FrameController(main_menu)
     controller.run()
