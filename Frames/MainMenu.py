4c2cb8c0c4e5b0371ec811269bb30a435d786839
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-01 18:29:29 +0300

    Update MainMenu.py

--- Frames/MainMenu.py
+++ Frames/MainMenu.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from IFrame import IFrame
 from Signals import KillEntireApp, NewFrame, KillTopFrame
-from utilities import draw_text, draw_fon
+from utilities import draw_text, load_image
 from Button import Button
 from Frames.Settings import Settings
 from Frames.ChooseMode import ChooseMode
@@ -12,38 +12,43 @@ class MainMenu(IFrame):
     def __init__(self):
         self.w = shared.WIDTH
         self.h = shared.HEIGHT
-        self.all_sprites = pg.sprite.Group()
-
-
-        shared.screen.fill(pg.Color('white'))
+        self.image_fon = pg.transform.scale(load_image('fon_menu.png'), (self.w, self.h))
+        self.buttons = pg.sprite.Group()
+        self.generate_buttons()
 
     def update(self):
-        for event in pg.event.get():
+        events = pg.event.get()
+        for event in events:
             if event.type == pg.QUIT:
                 raise KillEntireApp
-            if event.type == pg.MOUSEBUTTONDOWN:
-                for button in shared.all_buttons_cords:
-                    if button.get_rect().collidepoint(event.pos):
-                        if button.get_info() == 'settings':
-                            raise NewFrame(Settings())
-                        if button.get_info() == 'choose_mode':
-                            raise NewFrame(ChooseMode())
-                        if button.get_info() == 'leave':
-                            raise KillEntireApp
-                    if event.type == pg.KEYDOWN:
-                        if event.key == pg.K_ESCAPE:
-                            raise KillTopFrame
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    raise KillTopFrame
+        self.draw_fon()
+        self.buttons.update(events)
+        self.buttons.draw(shared.screen)
 
-        self.start_window()
-        self.all_sprites.draw(shared.screen)
-        self.all_sprites.update()
-        pg.display.flip()
+    def exit(self):
+        raise KillEntireApp
 
-    def start_window(self):
-        draw_fon('fon_menu.png', self.w, self.h)
+    def settings(self):
+        raise NewFrame(Settings())
+
+    def start_button(self):
+        raise NewFrame(ChooseMode())
+
+    def draw_fon(self):
+        shared.screen.blit(self.image_fon, (0, 0))
         draw_text('MyAntiyoy', self.w // 2.58, self.h // 4.25, '#00FF7F', int(self.w * 0.058))
 
-        Button(self.all_sprites, 'game_start_button.png', self.w * 0.46, self.h * 0.5, int(0.08 * self.w),
-               int(0.08 * self.w), 'choose_mode')
-        Button(self.all_sprites, 'settings_button.png', 0, 0, int(0.04 * self.w), int(0.04 * self.w), 'settings')
-        Button(self.all_sprites, 'leave_button.png', self.w * 0.958, 0, int(0.04 * self.w), int(0.04 * self.w), 'leave')
+    def generate_buttons(self):
+        exit_button = Button(
+            (self.w * 0.958, 0, int(0.04 * self.w), int(0.04 * self.w)), 'leave_button.png', self.buttons)
+        exit_button.connect(self.exit)
+
+        settings_button = Button((0, 0, int(0.04 * self.w), int(0.04 * self.w)), 'settings_button.png', self.buttons)
+        settings_button.connect(self.settings)
+
+        game_start_button = Button((self.w * 0.46, self.h * 0.5, int(0.08 * self.w), int(0.08 * self.w)),
+                                   'game_start_button.png', self.buttons)
+        game_start_button.connect(self.start_button)
