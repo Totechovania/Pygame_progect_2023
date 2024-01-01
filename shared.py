920a1c8ebefb6fa6a5d2df4e061d97fe0eec96e3
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-01 18:32:58 +0300

    Update shared.py

--- shared.py
+++ shared.py
@@ -1,11 +1,20 @@
 # файл, который содержит переменные, используемые во всем проекте
-# изменять только преднозначенными для этого функциями
+# изменять только предназначенными для этого функциями
 
 import pygame as pg
 
+__all__ = (
+    'WIDTH',
+    'HEIGHT',
+    'FPS',
+    'screen',
+    'clock',
+)
 
+FPS: int
 WIDTH: int
 HEIGHT: int
 screen: pg.Surface
 clock: pg.time.Clock
-FPS: int
+sound: bool
+music: bool
