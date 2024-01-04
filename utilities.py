18f03a5f1f59cac81605f990a09a73ad3c54729d
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-04 14:17:29 +0300

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,8 +4,10 @@ import shared
 import pygame as pg
 import os
 from PIL import Image, ImageFilter
-from Signals import *
+from pyautogui import screenshot
+from Signals import NewFrame, KillTopFrame, KillEntireApp
 from math import cos, sin, pi
+import json
 
 
 def terminate():
@@ -19,15 +21,19 @@ def apply_global_settings():
 
 
 def set_shared_variables():
-    display_info = pg.display.Info()
-    shared.WIDTH = display_info.current_w * 0.8
-    shared.HEIGHT = display_info.current_h * 0.8
-    shared.all_buttons_cords = []
-    shared.sound = True
-    shared.music = True
-    shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
-    shared.clock = pg.time.Clock()
-    shared.FPS = 60
+    with open('settings.json', 'r') as f:
+        f_data = json.load(f)
+        shared.fullscreen = f_data['FULLSCREEN']
+        shared.WIDTH = f_data['WIDTH']
+        shared.HEIGHT = f_data['HEIGHT']
+        shared.sound = f_data['SOUND']
+        shared.music = f_data['MUSIC']
+        if shared.fullscreen:
+            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT), pg.FULLSCREEN)
+        else:
+            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
+        shared.clock = pg.time.Clock()
+        shared.FPS = 60
 
 
 def load_image(name, colorkey=None):
@@ -53,27 +59,41 @@ def draw_text(text, x, y, color, size=50):
     shared.screen.blit(to_print, (x, y))
 
 
-def change_volume_settings():
-    if shared.sound:
-        shared.sound = False
-    else:
-        shared.sound = True
+def load_json_file():
+    with open('settings.json') as f:
+        return json.load(f)
 
 
-def change_music_settings():
-    if shared.music:
-        shared.music = False
-    else:
-        shared.music = True
+def change_json_file(data):
+    with open('settings.json', 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+def set_default_settings():
+    data = load_json_file()
+    data['SOUND'] = True
+    data['MUSIC'] = True
+    data['HEIGHT'] = 800
+    data['WIDTH'] = 2000
+    data['FULLSCREEN'] = True
+    change_json_file(data)
+    raise KillEntireApp
 
 
-def blur_image(img, amt=10):
-    raw_str = pg.image.tostring(img, 'RGBA', False)
-    img = Image.frombytes('RGBA', img.get_size(), raw_str)
-    img = img.filter(ImageFilter.GaussianBlur(amt))
-    raw_str = img.tobytes('raw', 'RGBA')
-    surf = pg.image.fromstring(raw_str, img.size, 'RGBA')
-    return surf
+def convert_image(filename):
+    with Image.open('data/' + filename) as img:
+        img.load()
+        img = img.filter(ImageFilter.GaussianBlur(20))
+        img.save('data/' + filename)
+
+
+def open_pop_window():
+    from Frames.PopUpWindow import PopUpWindow
+    screenshot('data/screenshot.png')
+    raise NewFrame(PopUpWindow('screenshot.png'))
+
+
+def back():
+    raise KillTopFrame
 
 
 def hexagon_from_center(center_x: float, center_y: float, radius: float) -> list[tuple[int, int]]:
@@ -86,5 +106,4 @@ def hexagon_from_center(center_x: float, center_y: float, radius: float) -> list
         y = start_x * sin(angle) + start_y * cos(angle)
         vertices.append((round(x + center_x),
                          round(y + center_y)))
-
     return vertices
