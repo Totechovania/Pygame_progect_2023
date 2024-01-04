12886172547dfd662e65f21cc99d48f9311d558b
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-04 22:00:18 +0300

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,9 +4,11 @@ import shared
 import pygame as pg
 import os
 from PIL import Image, ImageFilter
+from pyautogui import screenshot
 from Signals import NewFrame, KillTopFrame, KillEntireApp
 from math import cos, sin, pi
 import json
+import random
 
 
 def terminate():
@@ -20,6 +22,13 @@ def apply_global_settings():
 
 
 def set_shared_variables():
+    info = pg.display.Info()
+    w = info.current_w
+    h = info.current_h
+    if not os.path.exists('settings.json'):
+        with open('settings.json', 'w') as f:
+            json.dump({"FULLSCREEN": True, "WIDTH": w * 0.581, "HEIGHT": h * 0.55, "SOUND": True, "MUSIC": True}, f)
+
     with open('settings.json', 'r') as f:
         f_data = json.load(f)
         shared.fullscreen = f_data['FULLSCREEN']
@@ -27,10 +36,24 @@ def set_shared_variables():
         shared.HEIGHT = f_data['HEIGHT']
         shared.sound = f_data['SOUND']
         shared.music = f_data['MUSIC']
-        if shared.fullscreen:
-            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT), pg.FULLSCREEN)
-        else:
-            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
+        try:
+            if shared.fullscreen:
+                shared.screen = pg.display.set_mode((info.current_w, info.current_h))
+                shared.WIDTH = w
+                shared.HEIGHT = h
+            else:
+                shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
+        except Exception:
+            data = load_json_file()
+            data['HEIGHT'] = 800
+            data['WIDTH'] = 2000
+            change_json_file(data)
+            shared.WIDTH = f_data['WIDTH']
+            shared.HEIGHT = f_data['HEIGHT']
+            if shared.fullscreen:
+                shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT), pg.FULLSCREEN)
+            else:
+                shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
         shared.clock = pg.time.Clock()
         shared.FPS = 60
 
@@ -67,24 +90,36 @@ def change_json_file(data):
     with open('settings.json', 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
+
 def set_default_settings():
     data = load_json_file()
+    info = pg.display.Info()
+    w = info.current_w
+    h = info.current_h
     data['SOUND'] = True
     data['MUSIC'] = True
-    data['HEIGHT'] = 800
-    data['WIDTH'] = 2000
+    data['HEIGHT'] = h * 0.55
+    data['WIDTH'] = w * 0.581
     data['FULLSCREEN'] = True
     change_json_file(data)
     raise KillEntireApp
 
 
-def blur_image(img, amt=10):
-    raw_str = pg.image.tostring(img, 'RGBA', False)
-    img = Image.frombytes('RGBA', img.get_size(), raw_str)
-    img = img.filter(ImageFilter.GaussianBlur(amt))
-    raw_str = img.tobytes('raw', 'RGBA')
-    surf = pg.image.fromstring(raw_str, img.size, 'RGBA')
-    return surf
+def convert_image(filename):
+    with Image.open('data/' + filename) as img:
+        img.load()
+        img = img.filter(ImageFilter.GaussianBlur(20))
+        img.save('data/' + filename)
+
+
+def open_pop_window():
+    from Frames.PopUpWindow import PopUpWindow
+    screenshot('data/screenshot.png')
+    raise NewFrame(PopUpWindow())
+
+
+def back():
+    raise KillTopFrame
 
 
 def hexagon_from_center(center_x: float, center_y: float, radius: float) -> list[tuple[int, int]]:
@@ -97,5 +132,26 @@ def hexagon_from_center(center_x: float, center_y: float, radius: float) -> list
         y = start_x * sin(angle) + start_y * cos(angle)
         vertices.append((round(x + center_x),
                          round(y + center_y)))
-
     return vertices
+
+
+def blur_image(img, amt=10):
+    raw_str = pg.image.tostring(img, 'RGBA', False)
+    img = Image.frombytes('RGBA', img.get_size(), raw_str)
+    img = img.filter(ImageFilter.GaussianBlur(amt))
+    raw_str = img.tobytes('raw', 'RGBA')
+    surf = pg.image.fromstring(raw_str, img.size, 'RGBA')
+    return surf
+
+
+def create_particles(position, groop, name):
+    from Particles import Particle
+    particle_count = 5
+    numbers = [-6, -5, -4, -3, 3, 4, 5, 6]
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers), random.choice(numbers), name, groop)
+
+
+def get_size():
+    data = load_json_file()
+    return data['WIDTH'], data['HEIGHT']
