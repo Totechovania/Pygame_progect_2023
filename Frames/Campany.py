5c4971956f5a987aceed1115e5b4bc2c45966c3d
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-06 19:26:48 +0300

    Update Campany.py

--- Frames/Campany.py
+++ Frames/Campany.py
@@ -3,6 +3,7 @@ from Frames.IFrame import IFrame
 from utilities.Button import Button
 from utilities.Particles import create_particles
 from utilities.image import draw_text, load_image
+from utilities.music import play_sound
 import pygame as pg
 import shared
 from Signals import *
@@ -32,6 +33,19 @@ class Campany(IFrame):
         self.particles.update()
         self.particles.draw(shared.screen)
         self.buttons.draw(shared.screen)
+
+    def apply_settings(self):
+        self.buttons.empty()
+        if shared.fullscreen:
+            shared.screen = pg.display.set_mode((shared.fullscreen_w, shared.fullscreen_h), pg.FULLSCREEN)
+            shared.WIDTH = shared.fullscreen_w
+            shared.HEIGHT = shared.fullscreen_h
+        else:
+            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
+        self.w = shared.WIDTH
+        self.h = shared.HEIGHT
+        self.image_fon = pg.transform.scale(load_image('fon_menu.png'), (self.w, self.h))
+        self.generate_buttons()
 
     def go_to_first_level(self):
         pass
@@ -95,9 +109,11 @@ class Campany(IFrame):
         draw_text('5', self.w * 0.911, self.h * 0.33, int(self.w * 0.06))
 
     def back(self):
+        play_sound('button_press.mp3')
         raise KillTopFrame
 
     def open_pop_up_window(self):
+        play_sound('button_press.mp3')
         self.draw_fon()
         self.buttons.draw(shared.screen)
         raise NewFrame(PopUpWindow(shared.screen.copy()))
