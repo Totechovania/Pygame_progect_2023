ffe5a49b01725787978813df95863c4f630fe9b6
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-06 19:27:25 +0300

    Update FightMenuWindow.py

--- Frames/FightMenuWindow.py
+++ Frames/FightMenuWindow.py
@@ -3,6 +3,7 @@ from Frames.IFrame import IFrame
 from utilities.Button import Button
 from utilities.Particles import create_particles
 from utilities.image import load_image
+from utilities.music import play_sound
 import pygame as pg
 import shared
 from Signals import *
@@ -15,6 +16,19 @@ class FightMenuWindow(IFrame):
         self.image_fon = pg.transform.scale(load_image('fon_menu.png'), (self.w, self.h))
         self.buttons = pg.sprite.Group()
         self.particles = pg.sprite.Group()
+        self.generate_buttons()
+
+    def apply_settings(self):
+        self.buttons.empty()
+        if shared.fullscreen:
+            shared.screen = pg.display.set_mode((shared.fullscreen_w, shared.fullscreen_h), pg.FULLSCREEN)
+            shared.WIDTH = shared.fullscreen_w
+            shared.HEIGHT = shared.fullscreen_h
+        else:
+            shared.screen = pg.display.set_mode((shared.WIDTH, shared.HEIGHT))
+        self.w = shared.WIDTH
+        self.h = shared.HEIGHT
+        self.image_fon = pg.transform.scale(load_image('fon_menu.png'), (self.w, self.h))
         self.generate_buttons()
 
     def update(self):
@@ -45,9 +59,11 @@ class FightMenuWindow(IFrame):
         shared.screen.blit(self.image_fon, (0, 0))
 
     def back(self):
+        play_sound('button_press.mp3')
         raise KillTopFrame
 
     def open_pop_up_window(self):
+        play_sound('button_press.mp3')
         self.draw_fon()
         self.buttons.draw(shared.screen)
         raise NewFrame(PopUpWindow(shared.screen.copy()))
