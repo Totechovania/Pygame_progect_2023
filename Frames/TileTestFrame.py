bbd50b57b2d460001b5696c832fc9733f916f6a9
RomaMokrushin <120162552+RomaMokrushin@users.noreply.github.com>
2024-01-06 19:28:34 +0300

    Update TileTestFrame.py

--- Frames/TileTestFrame.py
+++ Frames/TileTestFrame.py
@@ -5,10 +5,16 @@ import shared
 from GameEngine.HexGrid import HexGrid
 import pygame as pg
 from GameEngine.Tile import HexTile, EmptyTile
+from utilities.Button import Button
+from utilities.music import play_sound
 
 
 class TileTestFrame(IFrame):
     def __init__(self):
+        self.w = shared.WIDTH
+        self.h = shared.HEIGHT
+        self.buttons = pg.sprite.Group()
+        self.generate_buttons()
         self.grid = HexGrid.filled(100, 50, 40,
                                    (20, 20, round(shared.WIDTH * 0.8), round(shared.HEIGHT * 0.8)))
         self.flag = False
@@ -16,7 +22,8 @@ class TileTestFrame(IFrame):
 
     def update(self):
         shared.screen.fill((255, 255, 255))
-        for event in pg.event.get():
+        events = pg.event.get()
+        for event in events:
             if event.type == pg.QUIT:
                 raise KillEntireApp
             if event.type == pg.MOUSEBUTTONDOWN:
@@ -48,15 +55,31 @@ class TileTestFrame(IFrame):
                 self.flag = True
         else:
             self.flag = False
-
         self.chosen = self.grid.collide_point(*pg.mouse.get_pos())
-
         self.grid.draw_tiles()
-
         if self.chosen is not None:
             self.chosen.draw_stroke(self.grid.surface)
             indexes = self.chosen.indexes
             for tile in self.grid.get_adjacent_tiles(indexes[0], indexes[1]):
                 tile.draw_stroke(self.grid.surface)
-
         self.grid.draw(shared.screen)
+        self.buttons.update(events)
+        self.buttons.draw(shared.screen)
+
+    def generate_buttons(self):
+        exit_button = Button(
+            (self.w * 0.958, 0, int(0.04 * self.w), int(0.04 * self.w)), 'leave_button.png', self.buttons)
+        exit_button.connect(self.open_pop_up_window)
+
+        back_button = Button((0, 0, int(0.04 * self.w), int(0.04 * self.w)), 'back.png', self.buttons)
+        back_button.connect(self.back)
+
+
+    def back(self):
+        play_sound('button_press.mp3')
+        raise KillTopFrame
+
+    def open_pop_up_window(self):
+        from Frames.PopUpWindow import PopUpWindow
+        play_sound('button_press.mp3')
+        raise NewFrame(PopUpWindow(shared.screen.copy()))
