f503c3841f2c84a69fea787064d85068b71844e0
Totechovania <bi068612@gmail.com>
2024-01-17 09:18:54 +0300

    Небольшие исправления.

--- Frames/ChooseLevelFrame.py
+++ Frames/ChooseLevelFrame.py
@@ -40,7 +40,7 @@ class ChooseLevelFrame(AbstractBaseFrame):
     def draw(self):
         draw_text('Выбранный уровень:', 0, self.w * 0.05, (0, 0, 0), round(self.h * 0.05))
         if self.chosen is not None:
-            draw_text(self.levels[self.chosen], self.w * 0.2, self.w * 0.05, (0, 0, 0), round(self.h * 0.05))
+            draw_text(self.levels[self.chosen], self.w * 0.25, self.w * 0.05, (0, 0, 0), round(self.h * 0.05))
         for i in range(len(self.levels)):
             draw_text(self.levels[i], self.w * 0.05 + (i // 10) * self.w * 0.4, self.w * 0.075 + self.w * 0.04 * (i % 10), (0, 0, 0),
                       round(self.h * 0.05))
